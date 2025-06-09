124489bf1531425c92b889995d51ea24e3374088
CorinaPC <corina@rede.ulbra.br>
2025-06-09 13:21:56 -0300

    conclusão do arquivo teste_app e comentação, aquivo app com comentario adicionado

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
-def somar(a, b):
+def somar(a, b): # nossa função de somar a+b que é puxada onde queremos testa-la ou usa-la
     return a + b
--- test_app.py
+++ test_app.py
@@ -1,6 +1,9 @@
-from app import somar
+from app import somar # importamos a função somar do arquivo app.py
 
 def test_somar():
-    assert somar(2, 3) == 5
+    assert somar(2, 3) == 5 # assert em Python é uma instrução para definir verificações de integridade em seu código
     assert somar(-1, 1) == 0
     assert somar(0, 0) == 0
+    print("Deu tudo certo !") # se passar por todos os assert sem dar erro e imprimir esse print então nossa função está funciondo perfeitamente !
+
+test_somar() # aqui chamamos nossa função para ela ser rodada
