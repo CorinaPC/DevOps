42c8a548596b6a0ab41145815aecab7bd299eac7
CorinaPC <corina@rede.ulbra.br>
2025-06-10 12:39:17 -0300

    colocando mais funções na calculadora

--- app.py
+++ app.py
@@ -1,2 +1,28 @@
-def somar(a, b): # nossa função de somar a+b que é puxada onde queremos testa-la ou usa-la
-    return a + b
+import math
+
+def somar(num_1,num_2):
+    return num_1+num_2
+
+def subtrair(num1,num2):
+    return num1-num2
+
+def multiplicacao(num_1,num_2):
+    return num_1*num_2
+
+def divisao(num_1,num_2):
+    return num_1/num_2
+
+def raiz_quadrada(num_1):
+    return math.sqrt(num_1)
+
+def potencia(num_1,num_2):
+    return math.pow(num_1,num_2)
+
+def seno(num_1):
+    return math.sin(num_1)
+
+def tangente(num_1):
+    return math.tan(num_1)
+
+def cosseno(num_1):
+    return math.cos(num_1)
--- test_app.py
+++ test_app.py
@@ -1,9 +1,4 @@
-from app import somar # importamos a função somar do arquivo app.py
-
-def test_somar():
-    assert somar(2, 3) == 5 # assert em Python é uma instrução para definir verificações de integridade em seu código
-    assert somar(-1, 1) == 0
-    assert somar(0, 0) == 0
-    print("Deu tudo certo !") # se passar por todos os assert sem dar erro e imprimir esse print então nossa função está funciondo perfeitamente !
-
-test_somar() # aqui chamamos nossa função para ela ser rodada
+from app import*
+num_1=int(input("Digite o valor principal: "))
+num_2=int(input("Digite outro valor: "))
+print(f"\n\tOperações matemáticas\n\nSomar: {somar(num_1,num_2)}\nSubtrair:{subtrair(num_1,num_2)}\nMultiplicacao: {multiplicacao(num_1,num_2)}\nDivisao:{divisao(num_1,num_2)}\nRaiz_quadrada: {raiz_quadrada(num_1)}\nPotencia:{potencia(num_1,num_2)}\n\n\tOperações trigonométricas\n\nSeno: {seno(num_1)}\nTangente: {tangente(num_1)}\nCosseno: {cosseno(num_1)}")
